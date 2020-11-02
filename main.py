235a7a29e2651128fe010b2663bc96e350bf3b8e
oyesil <oyesilo@yahoo.ca>
2020-11-01 23:21:27 -0500

    small updates fpr capturing faces

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
+    scaleFactor=1.15,
+    minNeighbors=4,    
     minSize=(30, 30),
     flags = cv2.CASCADE_SCALE_IMAGE
 )
